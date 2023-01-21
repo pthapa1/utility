84a379bed70bebae6acf1faa151241cae51ec6b4
Pratik Thapa <pratikthapa@Dark-Beauty-16.local>
2023-01-21 16:44:04 -0700

    oran Algeria complete

--- index.js
+++ index.js
@@ -8,71 +8,47 @@ const addressTypes = [
   'shopping centers',
   'things to do',
 ];
-
-const addressType = addressTypes[0]; // 0 - 3
-const country = 'Albania';
-const cities = ['Tirana', 'Durrës', 'Vlorë', 'Shkodër', 'Elbasan'];
-const city = cities[3]; // 0 - 4 . Max of index 4
-
-console.log(`Searching ${addressType} in ${city}, ${country} `);
-
-// used to replace ][ with a comma (,)
+const country = 'Algeria';
+const cities = ['Algiers', 'Oran', 'Constantine', 'Annaba', 'Batna'];
+const city = cities[1];
+const filePath = `./addresses/${country}.json`;
+// required by replace-in-file
 const options = {
   files: `./addresses/${country}.json`,
   from: /\]\s*\[/gm,
   to: ',',
   allowEmptyPaths: false,
 };
-
-const config = {
-  method: 'get',
-  url: `http://localhost:3000/address/${addressType}/${city}`,
-  headers: {},
+const writeAddressToFile = async (path, dataToWrite) => {
+  fs.appendFile(path, dataToWrite, (err) => {
+    if (err) throw err;
+    console.log(`Addresses Saved in ${country}.json file`);
+    // if this pattern -> ][ exists, replace it with a comma (,)
+    fs.existsSync(path)
+      ? replace.sync(options)
+      : console.log(`File does not exits yet`);
+  });
 };
 
-(async () => {
+const getCityAddresses = async (config) => {
   try {
     const response = await axios(config);
     const strRes = JSON.stringify(response.data);
-    // console.log(strRes);
-    fs.appendFile(`./addresses/${country}.json`, strRes, (err) => {
-      if (err) {
-        throw new err();
-      }
-      console.log('file created successfully.');
-    });
-
-    fs.existsSync(`./addresses/${country}.json`)
-      ? replace.sync(options)
-      : console.log(`File does not exits yet`);
+    await writeAddressToFile(filePath, strRes);
   } catch (error) {
-    console.error(error);
+    console.log(`Error in getCityAddress: ${error}`);
+  }
+};
+(async () => {
+  for (const addressType of addressTypes) {
+    console.log(`Searching ${addressType} in ${city}, ${country} `);
+    const config = {
+      method: 'get',
+      url: `http://localhost:3000/address/${addressType}/${city}`,
+      headers: {},
+    };
+    await getCityAddresses(config);
   }
 })();
 
-// fs.readFile('./sample-country.json', 'utf-8', async (err, data) => {
-//   if (err) throw err;
-//   const fileData = await JSON.parse(data); // entire list.
-// });
-
-// const config = {
-//   method: 'get',
-//   url: 'http://localhost:3000/address/Roseau Dominica',
-//   headers: {},
-// };
-
-// axios(config)
-//   .then((response) => {
-//     const resFromAPI = JSON.stringify(response.data);
-//     // console.log(JSON.stringify(resFromAPI));
-
-//     fs.writeFile('Dominica.json', resFromAPI, (err) => {
-//       if (err) {
-//         throw new err();
-//       }
-//       console.log('file created successfully.');
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+// now loop over cities too and get the addresses.
--- utility_code/2-getAddressFromAPI.js
+++ utility_code/2-getAddressFromAPI.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
-const country = 'Afghanistan';
+const country = 'Algeria';
 const path = `././addresses/${country}.json`;
 
-fs.readFile(path, 'utf-8', (err, data) => {
+fs.readFile(path, 'utf-8', async (err, data) => {
   if (err) throw err;
-  const addressList = JSON.parse(data);
-  console.log(addressList.length);
+  const addressList = await JSON.parse(data);
+  console.log(
+    `${country} has total of ${addressList.length} addresses in the file`
+  );
   // console.log(addressList[0].address.includes(country));
-  const filteredData = addressList.filter((item) => {
-    return item.address !== null && item.address.includes(country);
-  });
+  const filteredData = await addressList.filter(
+    (item) =>
+      item.address &&
+      item.address.includes(country) &&
+      (item.address.match(/,/g) || []).length > 1
+  );
   console.log(filteredData.length);
 
   fs.appendFileSync(path, JSON.stringify(filteredData));
